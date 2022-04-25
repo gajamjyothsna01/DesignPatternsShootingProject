1b957b14c2778ac0afab03e7ccdd748c27b0ad72
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-04-25 11:15:14 +0530

    Player movement and Animation is added.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float playerSpeed;
+    public float playerJumpForce;
+    public float rotationSpeed;
+   // Quaternion playerRotataion, camRotation;
+    public Camera cam;
+   
+    CapsuleCollider capsuleCollider;
+    public Animator animator;
+    int ammo = 100;
+    //int maxAmmo = 100;
+   // public Transform bulletLaunchPosition;
+    //int health = 100;
+    //int maxHealth = 100;
+   
+    // public SpriteRenderer sprite;
+    // public bool isGameWin = false;
+    //public GameObject target;
+    // public AudioClip audioClip;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();  
+       
+        //animator = GetComponent<Animator>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animator.SetBool("isWalking", true);
+      
+        /* if (Input.GetKeyDown(KeyCode.F))
+         {
+             animator.SetBool("", !animator.GetBool("IsAiming"));
+         }*/
+        if (Input.GetMouseButtonDown(0))
+        {
+             // animator.SetBool("isFiring", !animator.GetBool("isFiring"));
+                Debug.Log("Firing State");
+                animator.SetTrigger("isFiring");
+                Debug.Log("Player Hit Method");
+                Debug.Log("Ammo Firing" +ammo);
+                ammo--;
+                
+                //WhenPlayerHitEnemy();
+                audioSource.Play();
+               
+
+               // ammo = Mathf.Clamp(ammo - 10, 0, maxAmmo);
+                // Debug.Log(ammo);
+            
+
+
+        }
+      
+    }
+
+   
+
+   
+
+      
+   
+}
